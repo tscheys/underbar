2e1609d1e918f1143d3d8052b9ce868bacdb8f57
Scheysie <tim.scheys@gmail.com>
2015-07-01 11:05:05 +0200

    _identity and _last now passing all tests

--- src/underbar.js
+++ src/underbar.js
@@ -7,6 +7,7 @@
   // seem very useful, but remember it--if a function needs to provide an
   // iterator when the user does not pass one in, this will be handy.
   _.identity = function(val) {
+    return val;
   };
 
   /**
@@ -37,6 +38,7 @@
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
   _.last = function(array, n) {
+    return n === undefined ? array[array.length - 1] : array.slice(n > array.length ? 0 : (array.length - n) ,array.length)
   };
 
   // Call iterator(value, key, collection) for each element of collection.
